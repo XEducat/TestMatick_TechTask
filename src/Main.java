e1312af56020245fb5e37274d04dd8cf11d6899e
XEducat <sichevskiy2004@gmail.com>
2025-04-29 23:26:47 +0300

    Implementing the logic for entering a range of number of shapes Added: - Logic to read numbers from the console for easy definition of the range of the number of shapes.

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,21 @@
 import java.util.List;
+import java.util.Scanner;
 import Interfaces.Shape;
 
 public class Main {
     public static void main(String[] args) {
-        List<Shape> shapes = ShapeFactory.generateRandomShapes(5, 15);
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter minimum number of shapes: ");
+        int minShapes = scanner.nextInt();
+
+        System.out.print("Enter maximum number of shapes: ");
+        int maxShapes = scanner.nextInt();
+
+        List<Shape> shapes = ShapeFactory.generateRandomShapes(minShapes, maxShapes);
         printShapes(shapes);
+
+        scanner.close();
     }
 
     private static void printShapes(List<Shape> shapes) {
